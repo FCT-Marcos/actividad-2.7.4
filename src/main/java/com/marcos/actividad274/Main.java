faf71b0606d9c341800cf8d1f4f915fbdaf2d83c
fct-marcos <marcos.barrera.external@eviden.com>
2024-03-30 10:50:37 +0000

    Created main class

---
+++ src/main/java/com/marcos/actividad274/Main.java
@@ -0,0 +1,24 @@
+package com.marcos.actividad274;
+
+import java.util.logging.Logger;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.marcos.products.Producto;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext contexto = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Logger logger = Logger.getLogger(Main.class.getName());
+		
+        Producto portatil = contexto.getBean("portatil", Producto.class);
+        Producto movil = contexto.getBean("movil", Producto.class);
+
+        logger.info("Nombre Producto: " + portatil.getName() + ", Precio: " + portatil.getPrice());
+        logger.info("Nombre Producto: " + movil.getName() + ", Precio: " + movil.getPrice());
+
+        contexto.close();
+	}
+
+}
